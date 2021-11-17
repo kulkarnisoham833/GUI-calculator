061705ccf6312eca35f70195d61a14254a7ac3dc
Soham Kulkarni <63484051+kulkarnisoham833@users.noreply.github.com>
2021-11-17 10:24:21 -0800

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -29,58 +29,58 @@ def pressequal():
 gui = tk.Tk()
 gui.geometry("500x500")
 button1 = tk.Button(gui, text="1", width=10, height=5,
-                    bg="black", fg="white", command=lambda: press(1), font=tk.font.Font(size=16))
+                    bg="black", fg="white", command=lambda: press(1))
 button1.grid(row=0, column=0)
 button2 = tk.Button(gui, text="2", width=10, height=5,
-                    bg="black", fg="white", command=lambda: press(2), font=tk.font.Font(size=16))
+                    bg="black", fg="white", command=lambda: press(2))
 button2.grid(row=0, column=1)
 button3 = tk.Button(gui, text="3", width=10, height=5,
-                    bg="black", fg="white", command=lambda: press(3), font=tk.font.Font(size=16))
+                    bg="black", fg="white", command=lambda: press(3))
 button3.grid(row=0, column=2)
 button4 = tk.Button(gui, text="4", width=10, height=5,
-                    bg="black", fg="white", command=lambda: press(4), font=tk.font.Font(size=16))
+                    bg="black", fg="white", command=lambda: press(4))
 button4.grid(row=1, column=0)
 button5 = tk.Button(gui, text="5", width=10, height=5,
-                    bg="black", fg="white", command=lambda: press(5), font=tk.font.Font(size=16))
+                    bg="black", fg="white", command=lambda: press(5))
 button5.grid(row=1, column=1)
 button6 = tk.Button(gui, text="6", width=10, height=5,
-                    bg="black", fg="white", command=lambda: press(6), font=tk.font.Font(size=16))
+                    bg="black", fg="white", command=lambda: press(6))
 button6.grid(row=1, column=2)
 button7 = tk.Button(gui, text="7", width=10, height=5,
-                    bg="black", fg="white", command=lambda: press(7), font=tk.font.Font(size=16))
+                    bg="black", fg="white", command=lambda: press(7))
 button7.grid(row=2, column=0)
 button8 = tk.Button(gui, text="8", width=10, height=5,
-                    bg="black", fg="white", command=lambda: press(8), font=tk.font.Font(size=16))
+                    bg="black", fg="white", command=lambda: press(8))
 button8.grid(row=2, column=1)
 button9 = tk.Button(gui, text="9", width=10, height=5,
-                    bg="black", fg="white", command=lambda: press(9), font=tk.font.Font(size=16))
+                    bg="black", fg="white", command=lambda: press(9))
 button9.grid(row=2, column=2)
 button0 = tk.Button(gui, text="0", width=10, height=5,
-                    bg="black", fg="white", command=lambda: press(0), font=tk.font.Font(size=16))
+                    bg="black", fg="white", command=lambda: press(0))
 button0.grid(row=3, column=1)
 
 buttonclear = tk.Button(gui, text="clear", width=10, height=5,
-                        bg="black", fg="white", command=lambda: clear(), font=tk.font.Font(size=16))
+                        bg="black", fg="white", command=lambda: clear())
 buttonclear.grid(row=3, column=0)
 
 buttonequal = tk.Button(gui, text="=", width=10, height=5,
-                        bg="black", fg="white", command=lambda: pressequal(), font=tk.font.Font(size=16))
+                        bg="black", fg="white", command=lambda: pressequal())
 buttonequal.grid(row=3, column=2)
 
 buttonplus = tk.Button(gui, text="+", width=10, height=5,
-                       bg="black", fg="white", command=lambda: press("+"), font=tk.font.Font(size=16))
+                       bg="black", fg="white", command=lambda: press("+"))
 buttonplus.grid(row=0, column=4)
 
 buttonminus = tk.Button(gui, text="-", width=10, height=5,
-                        bg="black", fg="white", command=lambda: press("-"), font=tk.font.Font(size=16))
+                        bg="black", fg="white", command=lambda: press("-"))
 buttonminus.grid(row=1, column=4)
 
 buttonmult = tk.Button(gui, text="*", width=10, height=5,
-                       bg="black", fg="white", command=lambda: press("*"), font=tk.font.Font(size=16))
+                       bg="black", fg="white", command=lambda: press("*"))
 buttonmult.grid(row=2, column=4)
 
 buttondiv = tk.Button(gui, text="/", width=10, height=5,
-                      bg="black", fg="white", command=lambda: press("/"), font=tk.font.Font(size=16))
+                      bg="black", fg="white", command=lambda: press("/"))
 buttondiv.grid(row=3, column=4)
 
 equation = tk.StringVar()
